175a4afdac8381cfc302c53088d7d477238b28e9
maehara08 <aso.riiiku08@gmail.com>
2016-05-17 10:50:40 +0900

    add findview

--- app/src/main/java/com/example/riku_maehara/rxbinding/MainActivity.java
+++ app/src/main/java/com/example/riku_maehara/rxbinding/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.riku_maehara.rxbinding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AutoCompleteTextView emailEditText=(AutoCompleteTextView)findViewById(R.id.email);
+        EditText passwordText=(EditText)findViewById(R.id.password);
+        Button signinButton=(Button)findViewById(R.id.signin_button);
+
     }
 }
